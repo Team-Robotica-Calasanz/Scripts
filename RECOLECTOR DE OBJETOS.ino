3cfb1d71a79f8ff3c9852abfb48dea50c99ff72b
BrujoXT <91346719+BrujoXT@users.noreply.github.com>
2021-09-24 15:25:38 -0500

    diff --git a/RECOLECTOR DE OBJETOS.ino b/RECOLECTOR DE OBJETOS.ino --- RECOLECTOR DE OBJETOS.ino +++ RECOLECTOR DE OBJETOS.ino @@ -88,24 +88,12 @@  #define NOTE_D8  4699  #define NOTE_DS8 4978  #define REST      0
    -
    -// change this to make the song slower or faster
     int tempo = 114;
    -
    -// change this to whichever pin you want to use
     int buzzer = 8;
    -
    -// notes of the moledy followed by the duration.
    -// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
    -// !!negative numbers are used to represent dotted notes,
    -// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
     int melody[] = {
     
    -  // Never Gonna Give You Up - Rick Astley
    -  // Score available at https://musescore.com/chlorondria_5/never-gonna-give-you-up_alto-sax
    -  // Arranged by Chlorondria
     
       NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,4, //1
       NOTE_E5,-4, NOTE_FS5,-4, NOTE_A5,16, NOTE_G5,16, NOTE_FS5,8,
       NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,2,
    @@ -173,13 +161,9 @@
     
       NOTE_E5,4, NOTE_D5,2, REST,4
     };
     
    -// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
    -// there are two values per note (pitch and duration), so for each note there are four bytes
     int notes = sizeof(melody) / sizeof(melody[0]) / 2;
    -
    -// this calculates the duration of a whole note in ms
     int wholenote = (60000 * 4) / tempo;
     
     int divider = 0, noteDuration = 0;
     
    @@ -252,5 +236,5 @@
       }
       
       
     }
    -zzz
    +

--- RECOLECTOR DE OBJETOS.ino
+++ RECOLECTOR DE OBJETOS.ino
@@ -89,22 +89,10 @@
 #define NOTE_DS8 4978
 #define REST      0
 
-
-// change this to make the song slower or faster
 int tempo = 114;
-
-// change this to whichever pin you want to use
 int buzzer = 8;
-
-// notes of the moledy followed by the duration.
-// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
-// !!negative numbers are used to represent dotted notes,
-// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
 int melody[] = {
 
-  // Never Gonna Give You Up - Rick Astley
-  // Score available at https://musescore.com/chlorondria_5/never-gonna-give-you-up_alto-sax
-  // Arranged by Chlorondria
 
   NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,4, //1
   NOTE_E5,-4, NOTE_FS5,-4, NOTE_A5,16, NOTE_G5,16, NOTE_FS5,8,
@@ -174,11 +162,7 @@ int melody[] = {
   NOTE_E5,4, NOTE_D5,2, REST,4
 };
 
-// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
-// there are two values per note (pitch and duration), so for each note there are four bytes
 int notes = sizeof(melody) / sizeof(melody[0]) / 2;
-
-// this calculates the duration of a whole note in ms
 int wholenote = (60000 * 4) / tempo;
 
 int divider = 0, noteDuration = 0;
@@ -253,4 +237,4 @@ void loop() {
   
   
 }
-zzz
+
