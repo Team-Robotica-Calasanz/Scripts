175a62645a3209e8575e7aa38c5b198c8f7e9d41
Pablo L <pablolondonopineda@gmail.com>
2021-10-01 14:59:21 -0500

    Perfeccionamiento del código seguidor ver 1.0

--- seguidor/seguidor.ino
+++ seguidor/seguidor.ino
@@ -1,8 +1,9 @@
+//CALIBRACIÓN DE SENSORES
 int Der = 900;
 int Izq = 900;
 
-//Codigo para que gire hacia la izquierda en interseccion
-void giroIzquierda(){
+//Función para que gire hacia la izquierda en interseccion
+void giroIzq(){
     goForward(M1, M2);
     delay(200);
     turnLeft(M2, M1);
@@ -10,56 +11,65 @@ void giroIzquierda(){
     while (sensorRead(J1) < Der){
     turnLeft(M2, M1);
     }
-  }
+}
 
-//Codigo para que gire hacia la izquierda en interseccion
-void giroDerecha(){
+//Función para que gire hacia la izquierda en interseccion
+void giroDer(){
   goForward(M1, M2);
   delay(200);
   turnRight(M2, M1);
   delay(700);
-  while (sensorRead(J1) < Der){
+  while (sensorRead(J2) < Izq){
   turnRight(M2, M1);
   }
+}
+
+//Para imprimir lecturas del sensor infrarrojo
+void lecturas(){
+  Serial.print("J2 = ");
+  Serial.print(sensorRead(J2));
+  Serial.print("        ");
+  Serial.print("J1 = ");  
+  Serial.println(sensorRead(J1));
+  
+  delay(50);
 }
  
 
 
 void setup () {
   Serial.begin(9600);
-  motorSpeed(M1,63);
-  motorSpeed(M2,60);
+
+  //Motor derecho
+  motorSpeed(M1,53);
+  //Motor izquierdo
+  motorSpeed(M2,50);
 
 }
 void loop () {
-   Serial.print("J2 = ");
-  Serial.print(sensorRead(J2));
 
-  Serial.print("        ");
-  
-  Serial.print("J1 = ");  
-  Serial.println(sensorRead(J1));
-  
-  delay(50);
-  
- if (sensorRead(J1) < Der  && sensorRead(J2) < Izq) {
+  lecturas();
+
+  //Si la linea esta en el centro de los dos sensores
+  if (sensorRead(J1) < Der  && sensorRead(J2) < Izq) {
     goForward(M1,M2);
-  } 
- else if (sensorRead(J1) > Der && sensorRead(J2) < Izq) {
+  }
+
+  //si el sensor de la derecha lee blanco
+  else if (sensorRead(J1) > Der && sensorRead(J2) < Izq) {
     turnRight(M2 , M1);
-  } 
- else if (sensorRead(J1) < Der && sensorRead(J2) > Izq) {
+  }
+
+  //si el sensor de la izquierda lee blanco
+  else if (sensorRead(J1) < Der && sensorRead(J2) > Izq) {
       turnLeft(M2 , M1);
-  } 
+  }
 
+  //si los dos sensores leen blanco (intersección)
   else if (sensorRead(J1) > Der && sensorRead(J2) > Izq) {
-      giroDerecha();
-  } 
- /*else if (sensorRead(J1) > Der && sensorRead(J2) > Izq){
-  while (sensorRead(J2) > Izq){
-    turnLeft(M2, M1);*/
-  
-  
-  
+    //Intrucciones para girar a la izquierda, derecha o seguir derehco
+    //giroDer();
+    //giroIzq();
+    //goForward(M2, M1);
+  }
 }
-
